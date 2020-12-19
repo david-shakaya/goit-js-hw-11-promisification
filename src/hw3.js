0b8753ead8a3e81dd6339567376b5666b316d6ce
david-shakaya <devidoz666@gmail.com>
2020-12-19 15:51:57 +0200

    Ужас закончился

--- src/hw3.js
+++ src/hw3.js
@@ -5,29 +5,43 @@ const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-let time=0
+// let time=0
 
 const makeTransaction = (transaction) => {
   const delay = randomIntegerFromInterval(200, 500)
+  
   return new Promise((resolve, reject) => {
     
-    const canProcess = Math.random() > 0.3;
     setTimeout(() => {
+      const canProcess = Math.random() > 0.3;
 
-    if (canProcess) {
-      resolve(transaction.id, delay);
-     time = delay
-    } 
+        
+      if (canProcess) {
+        const data = ({ id:transaction.id, time:delay })
+       resolve(data)
+      }else {
+        reject(transaction.id);
+      }
+   
 
-    reject(transaction.id);
-
-  }, delay);
+    }, delay);
+    
   })
-
+  
 };
+// makeTransaction({ id: 70, amount: 150 }).then(delay => delay)
+//   .then(delay => console.log(delay))
 
+//   makeTransaction({ id: 71, amount: 150 }).then(delay => delay)
+//   .then(delay => console.log(delay))
 
-const logSuccess = (id) => {
+//   makeTransaction({ id: 72, amount: 150 }).then(delay => delay)
+//   .then(delay => console.log(delay))
+
+//   makeTransaction({ id: 73, amount: 150 }).then(delay => delay)
+// .then(delay => console.log(delay))
+
+const logSuccess = ({id, time }) => {
   console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
@@ -35,7 +49,7 @@ const logError = id => {
   console.warn(`Error processing transaction ${id}. Please try again later.`);
 };
 
-makeTransaction({ id: 70, amount: 150 })
+makeTransaction({ id: 70, amount: 150})
   .then(logSuccess)
   .catch(logError);
 
@@ -50,3 +64,9 @@ makeTransaction({ id: 72, amount: 75 })
 makeTransaction({ id: 73, amount: 100 })
   .then(logSuccess)
   .catch(logError);
+
+
+//    function logger(time) {
+//     console.log(`Лог каждые ${time}ms. - ${Date.now()}`)
+// }
+// const intervalId = setInterval(logger, 1000, 1000);
